25c3d2351b5c6904929685782e4a1f0769a18e99
G.subhedar <aayansubhedar1@gmail.com>
2026-03-09 15:18:22 +0530

    Create enquiry.js

---
+++ enquiry.js
@@ -0,0 +1,15 @@
+function submitForm(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let country=document.getElementById("country").value;
+
+if(name=="" || email=="" || country==""){
+alert("Please fill all required fields");
+return false;
+}
+
+alert("Thank you for your enquiry! Our team will contact you.");
+
+return true;
+}
